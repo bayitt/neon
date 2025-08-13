d5a7fc3742c99fd13bf9d1d2a63f7e72824c0ff6
Fambegbe Olamileke <olamileke.f@gmail.com>
2025-08-13 16:44:38 +0100

    feat: model setup

--- models/category.go
+++ models/category.go
@@ -1,5 +1,22 @@
-package models;
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Category struct {
-	
+	ID        uint      `gorm:"primaryKey;not null" json:"-"`
+	Uuid      uuid.UUID `gorm:"type:varchar(36);not null" json:"uuid"`
+	Name      string    `gorm:"unique;type:varchar(255);not null" json:"name"`
+	Slug      string    `gorm:"unique;type:varchar(255);not null" json:"slug"`
+	CreatedAt time.Time `gorm:"not null" json:"-"`
+	UpdatedAt time.Time `gorm:"not null" json:"-"`
+}
+
+func (category *Category) BeforeCreate(transaction *gorm.DB) (err error) {
+	category.Uuid = uuid.New()
+	return
 }
---
+++ models/subscriber.go
@@ -0,0 +1,9 @@
+package models
+
+import "time"
+
+type Subscriber struct {
+	ID        uint      `gorm:"primaryKey;not null" json:"-"`
+	Email     string    `gorm:"unique;type:varchar(100)" json:"email"`
+	CreatedAt time.Time `gorm:"not null" json:"-"`
+}
