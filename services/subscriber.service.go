88322f7ac6de353ea7e02f1197dad3667eecfc51
Fambegbe Olamileke <olamileke.f@gmail.com>
2025-08-27 21:05:44 +0100

    feat: implemented POST /subscribe route

---
+++ services/subscriber.service.go
@@ -0,0 +1,30 @@
+package services
+
+import (
+	"neon/dto"
+	"neon/models"
+	"neon/utilities"
+	"net/http"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type SubscriberService struct {
+	DB *gorm.DB
+}
+
+func (ss *SubscriberService) Create(csDto *dto.CreateSubscriberDto) (models.Subscriber, error) {
+	subscriber := models.Subscriber{Email: csDto.Email}
+	result := ss.DB.Clauses(clause.OnConflict{DoNothing: true}).Create(&subscriber)
+
+	if result.Error != nil {
+		return models.Subscriber{}, utilities.ThrowError(
+			http.StatusInternalServerError,
+			"INTERNAL_SERVER_ERROR",
+			result.Error.Error(),
+		)
+	}
+
+	return subscriber, nil
+}
